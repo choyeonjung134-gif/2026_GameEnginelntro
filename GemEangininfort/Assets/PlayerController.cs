cdc570c1a6ac835a069c0cda6dc2a3c7f721a731
choyeonjung134 <choyeonjung134@gmail.com>
2026-03-26 16:20:58 +0900

    플레이어 기본 움직임

---
+++ GemEangininfort/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed) //점프 버튼 누르면
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Death")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene("PlaySene_" + collision.name);
+        }
+    }
+    void Update()
+    {
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+     }
+}
+     
---
+++ GemEangininfort/Assets/TitleManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject helpPanel;
+    public void GameStart()
+    {
+        SceneManager.LoadScene("PlayScene_Door1");
+    }
+    public void OpenHelp()
+    {
+        helpPanel.SetActive(true);
+    }
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    public void ButtonLog()
+    {
+        Debug.Log("BUTTON CLICKED!");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
